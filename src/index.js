76bb16f26fb0f9195072d246fc7c273edeb259fc
Ilya Fadzeyeu <Elias.Fadeyev@gmail.com>
2018-02-23 21:29:29 +0300

    advanced-zeros/feature: solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function getZerosCount(number, base) {
   // your implementation
+  let arrZerosCount = [];
+  let simpleMult = base;
+  let objSimpleMult = {};
+  for (let i = 2; i <= simpleMult; i++) {
+    if (simpleMult % i === 0) {
+      if (objSimpleMult[i]) objSimpleMult[i]++;
+      else objSimpleMult[i] = 1;
+      simpleMult = simpleMult / i;
+      i--;
+    }
+  }
+
+  for (let mult in objSimpleMult) {
+    let zerosCount = 0;
+    for (let i = +mult; ; i = i * mult) {    
+      if (Math.floor(number / i) === 0) break;
+      zerosCount += Math.floor(number / i);
+    }
+    arrZerosCount.push(Math.floor(zerosCount / objSimpleMult[mult]));
+  }
+  
+  return Math.min.apply(null, arrZerosCount);
 }
